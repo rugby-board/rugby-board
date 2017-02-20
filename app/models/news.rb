b67213fd605638d2ca3b7633c6546f40d518f18f
David Zhang <crispgm@gmail.com>
2017-02-20 15:47:52 +0800

    Add tests for model

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,6 +1,8 @@
 class News < ApplicationRecord
   PAGINATION_STEP = 20.freeze
 
+  validates :title, length: {minimum: 2}
+
   def markdown_content
     require "kramdown"
     Kramdown::Document.new(content).to_html
--- test/models/news_test.rb
+++ test/models/news_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class NewsTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save news without title" do
+    news = News.new
+    assert_not news.save
+  end
+
+  test "should not save news if title is not long enough" do
+    news = News.new
+    news.title = "1"
+    assert_not news.save
+  end
 end
