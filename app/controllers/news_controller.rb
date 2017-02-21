2333afada36e972a0309824d800dd8e3695b22b5
David Zhang <crispgm@gmail.com>
2017-02-21 11:51:51 +0800

    Display channel and event on news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,31 +4,6 @@ class NewsController < ApplicationController
     check_token
     @token = token
 
-    @channel_list = [
-      ['News', 0],
-      ['Event', 1],
-      ['Live', 2],
-      ['Meetup', 3]
-    ].freeze
-
-    @event_list = [
-      ['None', 0],
-      ['Test Matches', 1],
-      ['SixNations', 2],
-      ['Rugby Championship', 3],
-      ['Super Rugby', 4],
-      ['Premiership', 5],
-      ['TOP 14', 6],
-      ['PRO 12', 7],
-      ['European Champions Cup', 8],
-      ['Rugby World Cup', 9],
-      ['British and Irish Lions', 10],
-      ['European Challenge Cup', 11],
-      ['Currie Cup', 12],
-      ['Top League', 13],
-      ['Rugby Sevens', 14]
-    ].freeze
-
     if params[:id] != nil
       @edit_news = News.find(params[:id].to_i)
     end
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,5 +1,31 @@
 class News < ApplicationRecord
+
   PAGINATION_STEP = 20.freeze
+  
+  CHANNEL_LIST = [
+    ['新闻', 0],
+    ['比赛', 1],
+    ['Live', 2],
+    ['Meetup', 3]
+  ].freeze
+
+  EVENT_LIST = [
+    ['无', 0],
+    ['Test Matches', 1],
+    ['SixNations', 2],
+    ['Rugby Championship', 3],
+    ['Super Rugby', 4],
+    ['Premiership', 5],
+    ['TOP 14', 6],
+    ['PRO 12', 7],
+    ['European Champions Cup', 8],
+    ['Rugby World Cup', 9],
+    ['British and Irish Lions', 10],
+    ['European Challenge Cup', 11],
+    ['Currie Cup', 12],
+    ['Top League', 13],
+    ['Rugby Sevens', 14]
+  ].freeze
 
   validates :title, length: {minimum: 2}
 
