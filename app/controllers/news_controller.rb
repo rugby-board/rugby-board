aba8bbac2ec7eacb88ee62ff200e55f1dc1f79b0
David Zhang <crispgm@gmail.com>
2017-03-05 11:44:32 +0800

    Add results

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,10 +4,10 @@ class NewsController < ApplicationController
   def list
     page = params[:p].to_i || 1
     page = 1 if page <= 0
-    total = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]]).count
+    total = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]], channel: 0).count
     start = (page - 1) * News::PAGINATION_STEP
 
-    @news = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]])
+    @news = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]], channel: 0)
                 .reverse_order
                 .limit(News::PAGINATION_STEP)
                 .offset(start)
@@ -18,6 +18,25 @@ class NewsController < ApplicationController
       :total_page => (total.to_f/News::PAGINATION_STEP).ceil
     }
     @page_title = "News | "
+  end
+
+  def results
+    page = params[:p].to_i || 1
+    page = 1 if page <= 0
+    total = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]], channel: 1).count
+    start = (page - 1) * News::PAGINATION_STEP
+
+    @news = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]], channel: 1)
+                .reverse_order
+                .limit(News::PAGINATION_STEP)
+                .offset(start)
+    
+    @page = {
+      :total => total,
+      :cur_page => page,
+      :total_page => (total.to_f/News::PAGINATION_STEP).ceil
+    }
+    @page_title = "Results | "
   end
 
   def item
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'live' => 'live#index'
   
   get 'news' => 'news#list'
+  get 'results' => 'news#results'
   get 'news/feed' => 'news#feed'
   get 'news/:id' => 'news#item'
 
