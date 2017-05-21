18d1c3cd197ac6cac99ec51c6e4e264bab2b4505
David Zhang <crispgm@gmail.com>
2017-05-21 12:27:21 +0800

    EasyInput tool

--- app/assets/javascripts/components/_rugby_dict_query.js.jsx
+++ app/assets/javascripts/components/_rugby_dict_query.js.jsx
@@ -6,8 +6,8 @@ var RugbyDictQuery = React.createClass({
     };
   },
   handleTranslate() {
-    $.getJSON('/api/v1/dict.json', {entry: this.state.translationInput}, (response) => { 
-      this.setState({ translationResult: response.result.join(' ') }) 
+    $.getJSON('/api/v1/dict.json', {entry: this.state.translationInput}, (response) => {
+      this.setState({ translationResult: response.result.join(' ') });
     });
   },
   handleChange(event) {
@@ -15,27 +15,23 @@ var RugbyDictQuery = React.createClass({
   },
   render() {
     return (
-      <div className="section">
-        <div className="section-wrap">
-          <div className="section-item">
-            <div className="section-title">
-              {this.props.title}
-            </div>
-            <div className="section-content">
-              <input
-                type="text"
-                className="dict-input"
-                value={this.state.translationInput}
-                onChange={this.handleChange}
-              />
-              <button onClick={this.handleTranslate}>
-                Translate
-              </button>
-            </div>
-            <div className="section-content" id="translate-result">
-              {this.state.translationResult}
-            </div>
-          </div>
+      <div>
+        <div className="section-title">
+          {this.props.title}
+        </div>
+        <div className="section-content">
+          <input
+            type="text"
+            className="dict-input"
+            value={this.state.translationInput}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleTranslate}>
+            Translate
+          </button>
+        </div>
+        <div className="section-content" id="translate-result">
+          {this.state.translationResult}
         </div>
       </div>
     )
