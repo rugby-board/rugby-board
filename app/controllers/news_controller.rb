3bbc0f8bab50680200e756aaba7b409290878988
David Zhang <crispgm@gmail.com>
2017-05-28 22:56:02 +0800

    Merge news and results & add news listed by event (#15)
    * Merge news & results
    
    * Support news list by event
    
    * Set titles

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,41 +2,37 @@ class NewsController < ApplicationController
   attr_reader :page_title
 
   def list
-    page = params[:p].to_i || 1
-    page = 1 if page <= 0
-    total = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]], channel: 0).count
+    # get channels
+    channel_id = get_channel_id(params[:channel])
+    if channel_id.nil?
+      redirect_to "/404"
+      return
+    end
+    # get events
+    event_id = get_event_id(params[:event_name])
+    # proceed params
+    page = get_page(params[:p])
     start = (page - 1) * News::PAGINATION_STEP
-
-    @news = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]], channel: 0)
-                .reverse_order
-                .limit(News::PAGINATION_STEP)
-                .offset(start)
+    # get news
+    if event_id.nil?
+      total = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]], channel: channel_id)
+                  .count
+      @news = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]], channel: channel_id)
+                  .reverse_order.limit(News::PAGINATION_STEP).offset(start)
+    else
+      total = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]], channel: channel_id, event: event_id)
+                  .count
+      @news = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]], channel: channel_id, event: event_id)
+                  .reverse_order.limit(News::PAGINATION_STEP).offset(start)
+    end
     
     @page = {
       :total => total,
       :cur_page => page,
       :total_page => (total.to_f/News::PAGINATION_STEP).ceil
     }
-    @page_title = "News | "
-  end
 
-  def results
-    page = params[:p].to_i || 1
-    page = 1 if page <= 0
-    total = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]], channel: 1).count
-    start = (page - 1) * News::PAGINATION_STEP
-
-    @news = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]], channel: 1)
-                .reverse_order
-                .limit(News::PAGINATION_STEP)
-                .offset(start)
-    
-    @page = {
-      :total => total,
-      :cur_page => page,
-      :total_page => (total.to_f/News::PAGINATION_STEP).ceil
-    }
-    @page_title = "Results | "
+    set_page_title(channel_id, event_id)
   end
 
   def item
@@ -47,5 +43,51 @@ class NewsController < ApplicationController
   def feed
     @news = News.where(status: [News::STATUS[:ok], News::STATUS[:highlighted]]).last(News::PAGINATION_STEP).reverse
     render "news/feed.xml"
+  end
+
+  private
+  def get_page(page_param)
+    page = page_param.to_i || 1
+    page = 1 if page <= 0
+    page
+  end
+
+  def get_channel_id(channel_param)
+    channel = channel_param.to_s
+    return nil if channel.blank?
+    
+    channel_id = nil
+    case channel
+    when "news"
+      channel_id = 0
+    when "results"
+      channel_id = 1
+    when "events"
+      channel_id = [0, 1]
+    else
+      return nil
+    end
+
+    channel_id
+  end
+
+  def get_event_id(event_name)
+    return nil if event_name.blank?
+    event_sym = event_name.to_sym
+    return News::EVENT_NAME_ID_MAP[event_sym] if News::EVENT_NAME_ID_MAP.key?(event_sym)
+
+    nil
+  end
+
+  def set_page_title(channel_id, event_id)
+    if channel_id == 1
+      @page_title = "新闻 | "
+    elsif channel_id == 2
+      @page_title = "比赛结果 | "
+    end
+
+    unless event_id.nil?
+      @page_title = "#{News::EVENT_LIST[event_id][0]} | "
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 
   get 'live' => 'live#index'
   
-  get 'news' => 'news#list'
-  get 'results' => 'news#results'
+  get 'news'    => 'news#list', :channel => "news"
+  get 'results' => 'news#list', :channel => "results"
+  get 'event/:event_name' => 'news#list', :channel => "events"
+
   get 'news/feed' => 'news#feed'
   get 'news/:id' => 'news#item'
 
