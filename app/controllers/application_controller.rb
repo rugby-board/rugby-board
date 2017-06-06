31f21283fc16b3fcca33ea5c72ee6d4830110d58
David Zhang <crispgm@gmail.com>
2017-06-06 19:54:54 +0800

    News API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,7 @@
-require "application_responder"
-
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from ActionController::RoutingError, with: :not_found
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
-  
-  self.responder = ApplicationResponder
-  respond_to :html
 
   protected
   def not_found
